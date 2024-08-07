77ad428157c34c541f4fd5fa48a2e7645870a186
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-08-07 08:12:53 +0545

    feat(ui): not found page

---
+++ apps/plain/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(): JSX.Element {
+  return (
+    <main className="container relative flex flex-col items-center justify-center">
+      <div className="-mt-14 flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-black md:text-7xl">404</h1>
+        <p className="text-sm md:text-lg">Page not found</p>
+      </div>
+    </main>
+  )
+}
