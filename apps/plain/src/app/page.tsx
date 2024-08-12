feb75f6ec277af035a10674c94c930ab01261918
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-08-12 21:28:52 +0545

    feat(ui): add greeting and short description

--- apps/plain/src/app/page.tsx
+++ apps/plain/src/app/page.tsx
@@ -1,3 +1,42 @@
 export default function Home(): JSX.Element {
-  return <h1 className="font-serif">Bhaskar Khoraja</h1>
+  const date = new Date()
+  const weekdayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ]
+  const time = date.getHours()
+  let greeting
+
+  if (time === 0) {
+    greeting = "Midnight"
+  } else if (time >= 1 && time <= 10) {
+    greeting = "Morning"
+  } else if (time === 11 || time === 12) {
+    greeting = "Noon"
+  } else if (time >= 13 && time <= 17) {
+    greeting = "Afternoon"
+  } else {
+    greeting = "Evening"
+  }
+
+  return (
+    <main className="container pt-10">
+      <section className="space-y-6">
+        <h3 className="text-lg font-bold text-foreground/80">
+          It&apos;s {weekdayNames[date.getDay()]}, <br />
+          Hope Your {greeting} Is Going Well!
+        </h3>
+        <h1 className="text-3xl font-bold">
+          Bhaskar is a Next.js developer who creates clean, stunning, and
+          user-friendly web applications. His commitment to best practices
+          ensures maintainable and scalable solutions.
+        </h1>
+      </section>
+    </main>
+  )
 }
