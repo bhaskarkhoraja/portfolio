a4d4681db67824057fc974e473072af67198e7a7
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-08-15 09:05:05 +0545

    feat: constants for animated technology

--- apps/plain/src/config/constants.ts
+++ apps/plain/src/config/constants.ts
@@ -2,3 +2,9 @@ export const PLAIN_URL = "http://localhost:3000" as const
 
 // Timeout for cursor to show animating blobs around it
 export const IDLE_CURSOR_TIMEOUT = 3000 as const
+
+// SVG size for animated technology that i worked with section
+export const SVG_SIZE = 30 as const
+
+// Delay for shooting svg in second in animated techonology
+export const SVG_DELAY_SHOT = 4 as const
