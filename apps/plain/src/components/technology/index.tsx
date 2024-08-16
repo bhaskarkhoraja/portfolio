1738c43bdb4c2cea1522b93b3f2c65f6dd96a0b9
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-08-16 10:12:03 +0545

    feat(ui): add technology cartridge

--- apps/plain/src/components/technology/index.tsx
+++ apps/plain/src/components/technology/index.tsx
@@ -8,6 +8,8 @@ import Cannon from "@/components/technology/cannon"
 import { DegreeProvider } from "@/components/technology/degree-provider"
 import TechnologyWrapper from "@/components/technology/technology-wrapper"
 
+import TechnologyCartridge from "./technology-cartridge"
+
 const Technology = (): JSX.Element => {
   const containerRef = useRef<HTMLDivElement>(null)
   const [containerDimension, setContainerDimension] = useState({
@@ -19,8 +21,8 @@ const Technology = (): JSX.Element => {
     if (containerRef.current) {
       setContainerDimension({
         // decreasing the width and height of container because svg has offsets
-        width: containerRef.current.offsetWidth - 20,
-        height: containerRef.current.offsetHeight - 13,
+        width: containerRef.current.offsetWidth - 21,
+        height: containerRef.current.offsetHeight - 14,
       })
     }
   }, [])
@@ -66,25 +68,37 @@ const Technology = (): JSX.Element => {
   ] as const
 
   return (
-    <div
-      className="relative h-96 w-full"
-      ref={containerRef}
-    >
-      <div className="absolute bottom-0 left-0">
-        <DegreeProvider>
-          <Cannon noOfTech={technologyData.length} />
-          {containerDimension.width !== 0 &&
-            containerDimension.height !== 0 &&
-            technologyData.map((tech) => (
-              <TechnologyWrapper
-                containerDimension={containerDimension}
-                delay={tech.id * SVG_DELAY_SHOT}
-                key={tech.id}
-              >
-                {tech.icon}
-              </TechnologyWrapper>
-            ))}
-        </DegreeProvider>
+    <div className="flex w-full flex-col-reverse lg:flex-row">
+      <div className="mt-2 flex h-16 flex-row gap-4 rounded-lg border p-4 lg:h-96 lg:w-16 lg:flex-col-reverse">
+        {technologyData.map((tech) => (
+          <TechnologyCartridge
+            delay={tech.id * SVG_DELAY_SHOT}
+            key={tech.id}
+          >
+            {tech.icon}
+          </TechnologyCartridge>
+        ))}
+      </div>
+      <div
+        className="relative h-96 w-full"
+        ref={containerRef}
+      >
+        <div className="absolute bottom-0 left-0">
+          <DegreeProvider>
+            <Cannon noOfTech={technologyData.length} />
+            {containerDimension.width !== 0 &&
+              containerDimension.height !== 0 &&
+              technologyData.map((tech) => (
+                <TechnologyWrapper
+                  containerDimension={containerDimension}
+                  delay={tech.id * SVG_DELAY_SHOT}
+                  key={tech.id}
+                >
+                  {tech.icon}
+                </TechnologyWrapper>
+              ))}
+          </DegreeProvider>
+        </div>
       </div>
     </div>
   )
