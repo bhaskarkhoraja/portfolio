871edffdd400e1baa789746f4a6337fffc0dfd8b
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-08-16 16:56:37 +0545

    style: change design to bar

--- apps/plain/src/components/nav/active-nav-indicator.tsx
+++ apps/plain/src/components/nav/active-nav-indicator.tsx
@@ -1,11 +1,21 @@
 "use client"
 
 import { usePathname } from "next/navigation"
-import { Dot } from "lucide-react"
+import { motion } from "framer-motion"
+
+import { cn } from "@/lib/utils"
 
 const ActiveNavIndicator = ({ link }: { link: string }): JSX.Element | null => {
   const pathname = usePathname()
-  return link === pathname ? <Dot className="size-10" /> : null
+
+  return link === pathname ? (
+    <motion.div
+      className={cn(
+        "absolute left-1 top-1.5 flex h-6 w-2 items-center justify-center rounded-sm bg-foreground"
+      )}
+      layoutId="active-nav"
+    />
+  ) : null
 }
 
 export default ActiveNavIndicator
