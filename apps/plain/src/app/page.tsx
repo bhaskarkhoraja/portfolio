e43622e02bb4761f116ccac4eec2645eddbd1e71
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-09-02 20:37:56 +0545

    style: remove text-center and text-start

--- apps/plain/src/app/page.tsx
+++ apps/plain/src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home(): JSX.Element {
           It&apos;s {weekdayNames[date.getDay()]}, <br />
           Hope Your {greeting} Is Going Well!
         </h3>
-        <h1 className="text-center text-2xl font-bold md:text-start md:text-3xl">
+        <h1 className="text-2xl font-bold md:text-3xl">
           Bhaskar is a Next.js developer who turns ideas into reality. With a
           passion for innovation, he crafts seamless web experiences.
         </h1>
