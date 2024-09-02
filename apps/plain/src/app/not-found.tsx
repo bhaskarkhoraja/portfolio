901ed854e36a6da55ad932eb016f769e26d1af23
Bhaskar Khoraja <bkhoraja@gmail.com>
2024-09-02 21:05:03 +0545

    style: height in responsive device

--- apps/plain/src/app/not-found.tsx
+++ apps/plain/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound(): JSX.Element {
   return (
-    <main className="container relative flex flex-col items-center justify-center">
+    <main className="container relative flex h-[calc(100dvh-5rem)] flex-col items-center justify-center md:h-dvh">
       <div className="-mt-14 flex flex-col items-center justify-center">
         <h1 className="text-5xl font-black md:text-7xl">404</h1>
         <p className="text-sm md:text-lg">Page not found</p>
